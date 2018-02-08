e99da2ac9eafb78bb6d6e5f7ff86d9ae8b7b324c
Manish Vashisth <ebriks.manish@gmail.com>
2018-02-08 18:16:07 +0530

    change model/user.js

--- model/user.js
+++ model/user.js
@@ -10,5 +10,21 @@ const user = mongoose.Schema({
         type : String
     }
 });
-
 const Users = module.exports =  mongoose.model('Users', user);
+
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
+
+// const user = new Schema({
+//     name : {
+//         type : String
+//     },
+//     email : {
+//         type : String
+//     },
+//     password : {
+//         type : String
+//     }
+// });
+
+// const Users = module.exports =  mongoose.model('Users', user);
