8a10e4d88ce8a2ce21426a49470d41d427a80e75
Manish Vashisth <ebriks.manish@gmail.com>
2018-02-08 19:35:58 +0530

    arrow function implemented

--- app.js
+++ app.js
@@ -22,18 +22,16 @@ app.post('/createuser', (req, res) => {
         email : req.body.Email,
         password : req.body.Password
     };
-    User.create(value, (err, records) => {
-        if(err) return res.json("err")
-        else return res.json(records)
-    })
+    User.create(value)
+    .then((records) => {return res.json(records)})
+    .catch((err) => {return res.json("err")})
 })
 
 app.get('/retrieveuser', (req, res) => {
     let value = {};
-    User.find(value, (err, records) => {
-        if(err) return res.json("err")
-        else return res.json(records)
-    })
+    User.find(value)
+    .then((records) => {return res.json(records)})
+    .catch((err) => {return res.json("err")})
 })
 
 app.put('/updateuserbyemail', (req, res) => {
@@ -44,21 +42,64 @@ app.put('/updateuserbyemail', (req, res) => {
         name : req.body.Name,
         password : req.body.Password
     };
-    User.update(findCriteria, updatedRecords, (err, records) => {
-        if(err) return res.json("err")
-        else return res.json(records)
-    })
+    User.update(findCriteria, updatedRecords)
+    .then((records) => {return res.json(records)})
+    .catch((err) => {return res.json("err")})
 })
 
 app.delete('/deleteuserbyemail', (req, res) => {
     let criteria = {
         email : req.body.Email
     };
-    User.remove(criteria, (err) => {
-        if(err) return res.json("err")
-        else return res.json("user deleted")
-    })
+    User.remove(criteria)
+    .then((records) => {return res.json("user deleted")})
+    .catch(() => {return res.json("err")})
 })
+
+
+// app.post('/createuser', (req, res) => {
+//     let value = {
+//         name : req.body.Name,
+//         email : req.body.Email,
+//         password : req.body.Password
+//     };
+//     User.create(value, (err, records) => {
+//         if(err) return res.json("err")
+//         else return res.json(records)
+//     })
+// })
+
+// app.get('/retrieveuser', (req, res) => {
+//     let value = {};
+//     User.find(value, (err, records) => {
+//         if(err) return res.json("err")
+//         else return res.json(records)
+//     })
+// })
+
+// app.put('/updateuserbyemail', (req, res) => {
+//     let findCriteria = {
+//         email : req.body.Email
+//     };
+//     let updatedRecords = {
+//         name : req.body.Name,
+//         password : req.body.Password
+//     };
+//     User.update(findCriteria, updatedRecords, (err, records) => {
+//         if(err) return res.json("err")
+//         else return res.json(records)
+//     })
+// })
+
+// app.delete('/deleteuserbyemail', (req, res) => {
+//     let criteria = {
+//         email : req.body.Email
+//     };
+//     User.remove(criteria, (err) => {
+//         if(err) return res.json("err")
+//         else return res.json("user deleted")
+//     })
+// })
 
 // app.post('/createuser', function(req, res){
 //     let value = {
